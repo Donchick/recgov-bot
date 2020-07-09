be6e9784984a5421f3f34032c8460fa844827911
Donat Travin <dtravin@google.com>
2020-07-09 16:40:19 -0700

    updated dates

--- app.js
+++ app.js
@@ -46,9 +46,6 @@ app.listen(3001, () => {
   const usersDB = addUser('donat', '123', [{"resource": "whatsapp", "path":"+79214420927"}]);
   subscriptionStorage.add({camps: [{
       campId: 232447,
-      dates: ['2020-07-10', '2020-07-11'],
-    }, {
-      campId: 232447,
       dates: ['2020-07-17', '2020-07-18'],
     }, {
       campId: 232447,
@@ -68,9 +65,6 @@ app.listen(3001, () => {
     }, {
       campId: 232449,
       dates: ['2020-07-31','2020-08-01'],
-    }, {
-      campId: 232450,
-      dates: ['2020-07-10', '2020-07-11'],
     }, {
       campId: 232450,
       dates: ['2020-07-17', '2020-07-18'],
@@ -83,6 +77,12 @@ app.listen(3001, () => {
     }, {
       campId: 232768,
       dates: ['2020-07-24','2020-07-25'],
+    }, {
+      campId: 232768,
+      dates: ['2020-07-17','2020-07-18'],
+    }, {
+      campId: 232768,
+      dates: ['2020-07-31','2020-08-01'],
     }], userId: usersDB.keys().next().value});
   const availabilityChecker = new AvailabilityChecker();
   WhatsAppNotifier.notify('+79214420927', 'Boss, advanced pinger is working!');
