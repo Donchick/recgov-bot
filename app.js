32f5dad301011de6cb8eb416811f0f77c85a5e54
donattravin@gmail.com <dtravin@google.com>
2021-04-18 22:51:50 -0700

    fixed port issue

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
+console.log(PORT);
 app.listen(PORT, () => {
   console.log("Server started!");
   const usersDB = addUser('donat', '123', [{"resource": "whatsapp", "path":"+79214420927"}]);
